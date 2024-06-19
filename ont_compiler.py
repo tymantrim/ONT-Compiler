30749a88ee1931cf46ffa0014154218ec6f413ee
Ty Trimble <tymantrim@gmail.com>
2024-06-18 21:29:03 -0500

    Adds information to entry

--- ont_compiler.py
+++ ont_compiler.py
@@ -11,8 +11,13 @@ with open("ont_form.csv", newline='') as form:
             continue
 
         if row[2] not in files:
-            files[row[2]] = open('OutsideNiceThings/' + '_'.join(row[2].split()) + '.rtf', 'w+', newline='')
-        files[row[2]].write("This is a test\n")
+            files[row[2]] = open('OutsideNiceThings/' + '_'.join(row[2].split()) + '.rtf', 'w', newline='')
+        files[row[2]].write(row[1]+'\n')
+        if row[3] == '':
+            files[row[2]].write("This person uploaded a file, their link is " + row[4])
+        else:
+            files[row[2]].write(row[3])
+        files[row[2]].write('\n\n\n')
     
     for file in files:
         files[file].close()
