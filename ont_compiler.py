ce27cad3ace70ad65c60d0117b088dcb91922872
Ty Trimble <tymantrim@gmail.com>
2024-06-18 21:11:40 -0500

    Opens and closes files for each counselor in OutsideNiceThings directory

--- ont_compiler.py
+++ ont_compiler.py
@@ -1,6 +1,19 @@
 import csv
+import os
 
+files = {}
+if not os.path.isdir("OutsideNiceThings"):
+    os.mkdir("OutsideNiceThings")
 with open("ont_form.csv", newline='') as form:
     reader = csv.reader(form, quotechar='"')
     for row in reader:
-        print(row)
+        if row[0] == 'Timestamp':
+            continue
+
+        if row[2] not in files:
+            files[row[2]] = open('OutsideNiceThings/' + '_'.join(row[2].split()) + '.csv', 'w', newline='')
+        files[row[2]].write("This is a test\n")
+    
+    for file in files:
+        files[file].close()
+
